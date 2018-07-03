aa84ee894d095497eb5e695d7b7025eb6c6b5863
Geoffrey Sechter <geoffrey.sechter@gmail.com>
2018-07-03 08:41:00 -0700

    basic functionality working
    need to sort through some issues not getting pipelined through and cache problems

--- packages/gatsby-source-wordpress/src/normalizeImages.js
+++ packages/gatsby-source-wordpress/src/normalizeImages.js
@@ -13,23 +13,6 @@ function toHast(html) {
   return fromParse5(p5);
 }
 
-
-/*
-// Somewhere (maybe):
-'query {
-   allWordpressWpMedia {
-     edges {
-       node {
-         localFile {
-           absolutePath
-         }
-         source_url
-       }
-     }
-   }
- }'
- */
-
 async function normalizeEntity(entity, media) {
   if (entity.__type !== 'wordpress__POST' && entity.__type !== 'wordpress__PAGE') {
     return entity;
@@ -39,55 +22,59 @@ async function normalizeEntity(entity, media) {
 
   selectAll('img[class*=wp-image]', ast)
     .forEach(element => {
-      console.info(`Element: \n ${element.properties}`)
       const localFile = media.find(
         mObj => element.properties.src === mObj.source_url
       )
-
       if (localFile) {
-      const defaults = {
-        maxWidth: 650,
-        wrapperStyle: ``,
-        backgroundColor: `white`,
-        linkImagesToOriginal: true,
-        showCaptions: false,
+        const defaults = {
+          maxWidth: 650,
+          wrapperStyle: ``,
+          backgroundColor: `white`,
+          linkImagesToOriginal: true,
+          showCaptions: false,
+        }
+
+        // const options = _.defaults(pluginOptions, defaults)
+        const options = _.defaults(defaults)
+
+        // Call Gatsby Img
+        if (localFile.fluid) {
+          const rawHTML = `
+          <div
+            class="gatsby-image-outer-wrapper"
+            style="position: relative; display: block; ${
+              options.wrapperStyle
+            }; max-width: 1000px; margin-left: auto; margin-right: auto;"
+          >
+            <div
+              class="gatsby-image-wrapper"
+              style="padding-bottom: 1rem; position: relative; bottom: 0; left: 0; background-image: url('${
+              localFile.fluid.base64
+            }'); background-size: cover; display: block;"
+            >
+              <div style="width: 100%; padding-bottom: ${100 / localFile.fluid.aspectRatio}%;"></div>
+              <img
+                class="gatsby-resp-image-image ${element.properties.className.join(' ')}"
+                style="width: 100%; height: 100%; margin: 0; vertical-align: middle; position: absolute; top: 0; left: 0; box-shadow: inset 0px 0px 0px 400px ${
+                  options.backgroundColor
+                };"
+                alt="${element.properties.alt ? element.properties.alt : ""}"
+                title="${localFile.title ? localFile.title : ``}"
+                src="${localFile.fluid.src}"
+                srcset="${localFile.fluid.srcSet}"
+                sizes="${localFile.fluid.sizes}"
+              />
+            </div>
+          </div>
+          `
+          const transformed = toHast(rawHTML);
+          const childElement = transformed.children.find(child => child.type === 'element');
+          element.type = 'element';
+          element.tagName = childElement.tagName;
+          element.properties = childElement.properties;
+          element.children = childElement.children;
+        }
       }
-
-      // const options = _.defaults(pluginOptions, defaults)
-      const options = _.defaults(defaults)
-
-      // Call Gatsby Img
-      let rawHTML = `
-      <span
-        class="gatsby-resp-image-wrapper"
-        style="position: relative; display: block; ${
-          options.wrapperStyle
-        }; max-width: 1000px; margin-left: auto; margin-right: auto;"
-      >
-        <span
-          class="gatsby-resp-image-background-image"
-          style="padding-bottom: 1rem; position: relative; bottom: 0; left: 0; background-image: url('${
-          localFile.fluid.base64
-        }'); background-size: cover; display: block;"
-        >
-          <img
-            class="gatsby-resp-image-image"
-            style="width: 100%; height: 100%; margin: 0; vertical-align: middle; position: absolute; top: 0; left: 0; box-shadow: inset 0px 0px 0px 400px ${
-              options.backgroundColor
-            };"
-            alt="${localFile.alt ? localFile.alt : "Im and image"}"
-            title="${localFile.title ? localFile.title : ``}"
-            src="${fallbackSrc}"
-            srcset="${localFile.fluid.srcSet}"
-            sizes="${localFile.fluid.sizes}"
-          />
-        </span>
-      </span>
-      `
-      }
-
-      element.properties.title = 'What An Image!';
-      element.properties.alt = 'What An Image!';
     });
 
   const html = astToHtml(ast);
@@ -97,22 +84,6 @@ async function normalizeEntity(entity, media) {
 }
 
 function normalizeImages(entities) {
-  /*
-  graphql(
-    `
-      allWordpressWpMedia {
-        edges {
-          node {
-            source_url
-            localFile {
-              absolutePath
-            }
-          }
-        }
-      }
-    `).then(result => {
-      console.info(result.data)
-    }) */
   const media = entities.filter(e => e.__type === `wordpress__wp_media`)
   return Promise.all(entities.map(e => normalizeEntity(e, media)));
 }
